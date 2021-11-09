8e158791d5ade7fd59375f4c1fe1e800158d0c8a
Alex Wang <a3803896@gmail.com>
2021-11-09 18:18:20 +0800

    結束

--- src/components/MySwiper.jsx
+++ src/components/MySwiper.jsx
@@ -18,13 +18,6 @@ export default function MySwiper({ className }) {
         />
         <p className='absolute z-10 text-white lg:text-2.5xl lg:leading-10 font-bold'>新北市 | 不厭亭</p>
       </SwiperSlide>
-      <SwiperSlide>
-        <img
-          src='https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80'
-          alt='新北市不厭亭相片'
-        />
-        <p className='absolute z-10 text-white lg:text-2.5xl lg:leading-10 font-bold'>新北市 | 不厭亭</p>
-      </SwiperSlide>
     </Swiper>
   );
 }
